88180e381a08f8f093ec5e08e38030c817392ca6
sandeep <sandeep.madishetty@flexydial.com>
2025-08-08 18:07:58 +0530

    Created Assignment1Datatype

---
+++ JavaTraining/src/assignment/Assignment1_DataType.java
@@ -0,0 +1,13 @@
+package assignment;
+
+public class Assignment1_DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1. What will be the temperature :25.5
+		float temperature = 25.5f;
+		System.out.println(temperature);
+
+	}
+
+}
