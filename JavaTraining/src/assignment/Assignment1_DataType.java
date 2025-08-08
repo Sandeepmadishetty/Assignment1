d89463cdf88e17cd079f3f298ed8a0d97385be04
sandeep <sandeep.madishetty@flexydial.com>
2025-08-08 18:54:39 +0530

    completed Assignement1 Datatype

--- JavaTraining/src/assignment/Assignment1_DataType.java
+++ JavaTraining/src/assignment/Assignment1_DataType.java
@@ -1,5 +1,7 @@
 package assignment;
 
+import java.math.BigDecimal;
+
 public class Assignment1_DataType {
 
 	public static void main(String[] args) {
@@ -7,10 +9,79 @@ public class Assignment1_DataType {
 		// 1. What will be the temperature :25.5
 		float temperature = 25.5f;
 		System.out.println(temperature);
+		
 		//2. What will be the datatype for orderplaced true/ false
 		boolean orderPlaced = true;
 		System.out.println(orderPlaced);
-
+		
+		//3. Person's phone number = "123-456-7890"
+		String phoneNumber = "123-456-7890";
+		System.out.println(phoneNumber);
+		
+		//4. Amount of money in a customer bank account:1000.50
+		BigDecimal balance = new BigDecimal("1000.50");
+		System.out.println(balance);
+		
+		//5. Persons Email Address: "john.doe@example.com"
+		String email = "john.doe@example.com";
+		System.out.println(email);
+		
+		//Coordinate of location(latitude,Longitude):37.7749, -122.4194
+		double latitude = 37.7749; double longitude = -122.4194;
+		System.out.println(latitude);
+		
+		//Person MaritalStatus: true or false
+		boolean maritalstatus = true;
+		System.out.println(maritalstatus);
+		
+		//Person occupation
+		String occupation = "Software Engineer";
+		System.out.println(occupation);
+		
+		//Person favouritecolour
+		String favouritecolour = "Blue";
+		System.out.println(favouritecolour);
+		
+		//Current Year
+		int currentYear = 2023;
+		System.out.println(currentYear);
+		
+		// No of followers on social media platform:1000000
+		int followers = 1000000;
+		System.out.println(followers);
+		
+		// Rating of a movie
+		double rating = 7.5;
+		System.out.println(rating);
+		
+		// Person blood type:A
+		String bloodtype = "A";
+		System.out.println(bloodtype);
+		
+		//Title of a book: To Kill a Mockingbird
+		String book = "To Kill a Mockingbird";
+		System.out.println(book);
+		// No of Employee : 500
+		int employees = 500;
+		System.out.println(employees);
+		//Time of a event:2:30PM
+		String event = "2:30PM";
+		System.out.println(event);
+		
+		// Name of the country: "United States"
+		String country = "United states";
+		System.out.println(country);
+		
+		//Person Eye color: "Brown"
+		String eyecolor= "Brown";
+		System.out.println(eyecolor);
+		//Person birthplace: "New York City"
+		String birthplace = "New York City";
+		System.out.println(birthplace);
+		// Distance between two cities:200.5
+		float distance = 200.5f; 
+		System.out.println(distance);
+		
 	}
 
 }
